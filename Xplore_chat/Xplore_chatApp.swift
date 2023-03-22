c7a71643168f9fc9125651032a31c85035ca93c1
Valentina <balendi.art@gmail.com>
2023-03-22 09:50:10 +0100

    Initial Commit

---
+++ Xplore_chat/Xplore_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xplore_chatApp.swift
+//  Xplore_chat
+//
+//  Created by Apprenant 18 on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Xplore_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
